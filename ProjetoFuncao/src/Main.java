1fa2202d76950abdd656a812dabdff10351991a6
unknown <yandumarques@gmail.com>
2025-02-26 23:57:53 -0300

    Comitando metodo de faotiral

--- ProjetoFuncao/src/Main.java
+++ ProjetoFuncao/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
     }
+
+    public static int fatorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
 }
+
