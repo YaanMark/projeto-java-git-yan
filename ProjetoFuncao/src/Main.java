043e131fb39dea063e1a8ef0021c72ad09a0c853
unknown <yandumarques@gmail.com>
2025-02-27 00:15:41 -0300

    Comitando método de contar vogais

--- ProjetoFuncao/src/Main.java
+++ ProjetoFuncao/src/Main.java
@@ -2,4 +2,16 @@ public class Main {
     public static void main(String[] args) {
         
     }
+
+    public static int contVogais(String s) {
+        int counter = 0;
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 }
