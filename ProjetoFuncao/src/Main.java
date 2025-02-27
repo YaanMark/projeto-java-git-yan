ab1a228db24cd18217937b1ea4a41b820131dec3
unknown <yandumarques@gmail.com>
2025-02-27 00:03:11 -0300

    Comitando método de inverter string

--- ProjetoFuncao/src/Main.java
+++ ProjetoFuncao/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+
     }
+
+    public static String inverteString(String s) {
+
+        s = new StringBuilder(s).reverse().toString();
+        return s;
+    }
+
 }
