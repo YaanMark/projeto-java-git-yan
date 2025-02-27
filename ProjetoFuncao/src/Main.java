efe63dd57357fe4db84fae9b0a826530ffb33448
unknown <yandumarques@gmail.com>
2025-02-27 00:09:11 -0300

    Comitando metodo de media aritmética

--- ProjetoFuncao/src/Main.java
+++ ProjetoFuncao/src/Main.java
@@ -2,4 +2,10 @@ public class Main {
     public static void main(String[] args) {
         
     }
+
+    public static double media(int num1, int num2, int num3, int num4) {
+        double result = (num1 + num2 + num3 + num4) / 4;
+        return result;
+    }
+
 }
