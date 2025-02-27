7e7eab6d400c7bd725b06ef87c59f294fba37bf6
unknown <yandumarques@gmail.com>
2025-02-26 23:41:20 -0300

    Comitando metodo de verifação par

--- ProjetoFuncao/src/Main.java
+++ ProjetoFuncao/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        
+
     }
+
+    public static boolean isPar(int num){
+        if(num % 2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 }
